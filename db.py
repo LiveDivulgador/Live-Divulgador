298e48127ed2688a2cb39bc1471159a9226c4a67
Flavio Filipe <flaviofilipe.dev@gmail.com>
2021-03-04 09:46:43 -0300

    Adicionando excecoes de conexao ao banco

--- db.py
+++ db.py
@@ -1,23 +1,41 @@
-from sqlalchemy import create_engine, MetaData, Column
-from sqlalchemy.types import Boolean
-import pandas as pd
 import os
+from sqlalchemy import create_engine, MetaData
+from psycopg2 import OperationalError as PostgreSqlError
+from sqlalchemy.exc import OperationalError as SqlAlchemyError
 
-engine = create_engine("postgres://{}:{}@localhost:5432/streamers".format(
-    os.getenv("user_db"), os.getenv("passwd_db")))
+from dotenv import load_dotenv
 
-engine.execute("CREATE TABLE IF NOT EXISTS livecoders (Nome varchar(50), Id integer,\
-	Twitch varchar(150), Twitter varchar(50), OnStream boolean, Print boolean,\
-	Tipo varchar(5), Hashtags varchar(300))")
+load_dotenv()
 
-# Guardar objeto da table livecoders
-metadata = MetaData(bind=engine, reflect=True)
+user_db = os.getenv('user_db')
+passwd_db = os.getenv('passwd_db')
 
-livecoders = metadata.tables["livecoders"]
+if user_db is None or user_db == '':
+    raise ValueError('user_db não encontrado')
+
+if passwd_db is None or passwd_db == '':
+    raise ValueError('passwd_db não encontrado')
+
+try:
+    engine = create_engine("postgres://{}:{}@localhost:5432/streamers".format(user_db, passwd_db))
+
+    engine.execute("CREATE TABLE IF NOT EXISTS livecoders (Nome varchar(50), Id integer,\
+    Twitch varchar(150), Twitter varchar(50), OnStream boolean, Print boolean,\
+    Tipo varchar(5), Hashtags varchar(300))")
+
+    # Guardar objeto da table livecoders
+    metadata = MetaData(bind=engine, reflect=True)
+
+    livecoders = metadata.tables["livecoders"]
+except PostgreSqlError as e:
+    print(e)
+    exit()
+except SqlAlchemyError as e:
+    print(e)
+    exit()
 
 
 def returnStreamerInfo():
-
     # Retonar as os valores das colunas de todos os streamers
     result = engine.execute("SELECT * FROM livecoders")
 
@@ -25,7 +43,6 @@ def returnStreamerInfo():
 
 
 def returnStreamerNames():
-
     # Retorna o nome dos streamers
     result = engine.execute("SELECT Nome FROM livecoders")
 
@@ -33,10 +50,9 @@ def returnStreamerNames():
 
 
 def insertStreamers(streamers):
-
     # Insere novos streamers na DB
     for index, row in streamers.iterrows():
-        ins = livecoders.insert()\
+        ins = livecoders.insert() \
             .values(nome=row["Nome"], id=int(row["Id"]), twitch=row["Twitch"],
                     twitter=row["Twitter"], onstream=row["OnStream"], print=row["Print"],
                     tipo=row["Tipo"], hashtags=row["Hashtags"])
